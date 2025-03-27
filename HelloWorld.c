576172b02e12206e4a06ebfbda1b1249ce86b00c
Brinda Moudgalya <brindamoudgalya05@gmail.com>
2025-03-27 15:24:43 -0700

    added chapter 2 projects

--- HelloWorld.c
+++ HelloWorld.c
@@ -2,13 +2,11 @@
 /* this C file is for doing problems */
 
 #include <stdio.h> // preprocessor command, tells processor what to include
+#include <math.h>
 
 int main() {
 
-    int a; float b; int c;
-    a = 25; b = 3.24; c = a + b * b - 34;
-
-    printf("%d, %f, %d", a, b, c);
+    
 
     return 0; // return 0 if successful run, 1 if unsuccessful (or a non-zero number)
 }
--- ProjectsLetUsC.c
+++ ProjectsLetUsC.c
@@ -1,5 +1,69 @@
 
 # include <stdio.h>
+#include <math.h>
+
+void problem2_3() {
+    // compute smallest number of notes that will combine to give N rupees/dollars
+    // given denominations of Rs/$ are 1, 2, 5, 10, 50, and 100
+
+    int n;
+    printf("Enter the number of dollars you want to break down: ");
+    scanf(" %d", &n);
+
+    int d1, d2, d5, d10, d50, d100;
+    d100 = n/100;
+    n = n%100;
+    d50 = n/50;
+    n = n%50;
+    d10 = n/10;
+    n = n%10;
+    d5 = n/5;
+    n = n%5;
+    d2 = n/2;
+    n = n%2;
+    d1 = n;
+    printf("You can break this into %d $100 bills, %d $50 bills, %d $10 bills, ", d100, d50, d10);
+    printf("%d $5 bills, %d $2 bills, and %d $1 bills.", d5, d2, d1);
+    printf("\nThat's a total of %d bills.", d1+d2+d5+d10+d50+d100);
+}
+
+void problem2_2() {
+    // given a 5-digit number (user input), reverse the number
+
+    int num;
+    printf("Please enter a five-digit number: ");
+    scanf(" %d", &num);
+
+    int rev1, rev2, rev3, rev4, rev5;
+    rev5 = num/10000;
+    num = num % 10000;
+    rev4 = num/1000;
+    num = num % 1000;
+    rev3 = num/100;
+    num = num % 100;
+    rev2 = num/10;
+    num = num % 10;
+    rev1 = num;
+
+    printf("Your reversed number is: %i%i%i%i%i", rev1, rev2, rev3, rev4, rev5);
+
+}
+
+void problem2_1() {
+    // find the area of a triangle given its three side lengths
+    // area = sqrt(s(s-a)(s-b)(s-c)) given s = (a+b+c)/2
+
+    float a, b, c;
+
+    printf("Enter three side lengths: ");
+    scanf(" %f %f %f", &a, &b, &c);
+
+    float s = (a+b+c)/2;
+    float area = sqrtf(s * (s-a) * (s-b) * (s-c));
+
+    printf("The area is: %f", area);
+    
+}
 
 void problem1_2() {
     // calculate the distance between two cities in m, ft, in, cm (it'll be given in km)
@@ -41,8 +105,7 @@ void problem1_1() {
 
 int main() {
 
-    problem1_2();
-    // problem1_1();
+    problem2_3();
 
     return 0;
 }
